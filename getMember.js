d7c7d8d94120c4b5221de68db18774649315b8de
moonyoulove <charlie851213@gmail.com>
2018-05-07 17:12:14 +0800

    Update getMember.js

--- getMember.js
+++ getMember.js
@@ -1,20 +1,13 @@
 var iframe = document.createElement("iframe");
 iframe.src = "https://moonyoulove.github.io/get_qun_member/";
-iframe.style = "position: fixed;left: 35%;top: 20%;width: 250px;height: 350px;";
+iframe.style = "position: fixed;left: 35%;top: 20%;width: 250px;height: 350px;background:white;";
 document.body.appendChild(iframe);
 iframe.onload = function() {
 	var bkn = parent.$.getCSRFToken();
 	var gc = parent.document.URL.split("#gid=")[1];
 	var qun_url = "https://qun.qq.com/cgi-bin/qun_mgr/search_group_members?st=0&end=1000";
-	var logo_url = "https://ptlogin2.qq.com/getface?&imgtype=4&uin=";
 	var xhr = new XMLHttpRequest();
-	xhr.open("get", `$ {
-		qun_url
-	} & bkn = $ {
-		bkn
-	} & gc = $ {
-		gc
-	}`, true);
+	xhr.open("get", `${qun_url}&bkn=${bkn}&gc=${gc}`, true);
 	xhr.send();
 	xhr.onreadystatechange = function() {
 		if (xhr.readyState == 4 && xhr.status == 200) {
